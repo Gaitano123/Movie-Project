51581210bd52476da57a9f23cb46fb7b020304fe
Gaitano <gaitano.george@student.moringaschool.com>
2023-07-05 12:21:53 +0300

    Dispalying the searched movies in the DOM

--- index.js
+++ index.js
@@ -22,5 +22,59 @@ document.addEventListener('DOMContentLoaded', () => {
         detailContainer.innerHTML = ''
     })
 
+    function displayMovie(movie){
+        const movieContainer = document.createElement('div')
+        movieContainer.className="movie-container"
+        movieContainer.addEventListener('click', () => hoveringMovie(movie.imdbID))
+        resultContainer.appendChild(movieContainer)
 
+        const moviePoster = document.createElement('img')
+        moviePoster.src = movie.Poster;
+        moviePoster.className="movie-poster"
+        movieContainer.appendChild(moviePoster)
+
+        const movieTitle = document.createElement('p');
+        movieTitle.innerText = movie.Title
+        movieTitle.className="movie-title"
+        movieContainer.appendChild(movieTitle)
+
+        const movieType = document.createElement('p')
+        movieType.innerText = "Type: " + movie.Type;
+        movieType.className="movie-type"
+        movieContainer.appendChild(movieType)
+
+        const movieId = movie.imdbID;
+
+        const movieYear = document.createElement('p');
+        movieYear.innerText = "Year: " + movie.Year;
+        movieYear.className="movie-year"
+        movieContainer.appendChild(movieYear)
+
+        const movieButton = document.createElement('button');
+        movieButton.innerText = 'favourites';
+        movieButton.className="movie-button";
+        movieButton.addEventListener('click', () => {
+            addFavouriteMovie(movie)
+        })
+        movieContainer.appendChild(movieButton)
+        
+        const movieTrailer = document.createElement('a')
+        const filmName = movie.Title
+        movieTrailer.href = `https://www.youtube.com/results?search_query=${filmName}+trailer`
+        movieTrailer.target = "_blank"
+        movieTrailer.innerText = 'trailer';
+        // <iframe width="853" height="480" src="https://www.youtube.com/embed/gaBdgu00otE" title="Ip Man 2: Legend of the Grandmaster OFFICIAL TRAILER" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        movieTrailer.className="movie-trailer"
+        movieContainer.appendChild(movieTrailer)
+
+        // const movieVideo = document.createElement('iframe')
+        // movieVideo.src = `https://www.youtube.com/embed/gaBdgu00otE`
+        // movieVideo.className = "movie-video"
+        // movieVideo.innerText= "Movie"
+        // movieContainer.appendChild(movieVideo)
+
+        return movieId
+    }
+
+    
 })
